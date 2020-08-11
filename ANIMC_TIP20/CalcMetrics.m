db498989c3dedebd4bf4c6a836f1aa29ccb79968
ZeusDavide <32944669+ZeusDavide@users.noreply.github.com>
2020-08-11 14:47:07 +0800

    Delete CalcMetrics.m

--- ANIMC_TIP20/CalcMetrics.m
+++
@@ -1,7 +0,0 @@
-function [AC, nmi_value, error_cnt] = CalcMetrics(label, result)
-% Written by Jialu Liu
-result = bestMap(label, result);
-error_cnt = sum(label ~= result);
-AC = length(find(label == result))/length(label);
-
-nmi_value = nmi(label, result);
